55e4c856c7810587983de1347b0aef1c4537d107
Maximilian Alexander <mbalex99@gmail.com>
2017-08-03 00:34:55 -0700

    adding large mutation for studio

--- src/faker.js
+++ src/faker.js
@@ -8,21 +8,44 @@ const ProductSchema = {
         productId: { type: 'int', optional: false },
         name: { type: 'string', optional: false },
         price: { type: 'float', optional: false },
-        companyName: { type: 'string', optional: false },
     }
 }
 
-const realm = new Realm({
-    schema: [ProductSchema]
-})
+const HOSTNAME = `192.241.226.174`
+const USERNAME = `af@realm.io`
+const PASSWORD = `test`
 
-for (var index = 0; index < 100; index++) {
-    realm.write(() => {
-        realm.create('Product', {
-            productId: index,
-            name: faker.commerce.product.name,
-            price: faker.random.number({min: 20, max: 2000}),
-            companyName: faker.company.companyName()
-        }, true)
+if (Realm.Sync.User.current) {
+    startItUp(Realm.Sync.User.current)
+} else {
+    Realm.Sync.User.login(`http://${HOSTNAME}:9080/`, USERNAME, PASSWORD, (err, user) => {
+        if (err) { console.error(err) }
+        else {
+            startItUp(user)
+        }
     })
 }
+
+function startItUp(user) {
+    const realm = new Realm({
+        schema: [ProductSchema],
+        sync: {
+            user: user,
+            url: `realm://${HOSTNAME}:9080/productslarge`
+        }
+    });
+    setInterval(() => {
+        realm.write(() => {
+            for (var index = 0; index < 400000; index++) {
+                realm.create('Product', {
+                    productId: index,
+                    name: faker.commerce.product(),
+                    price: faker.random.number({ min: 100, max: 4000 })
+                }, true)
+            }
+        })
+    }, 2000)
+
+}
+
+
