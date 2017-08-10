e4bba60daa986d86c2dbe78f554e74b1d38cc0c5
Maximilian Alexander <mbalex99@gmail.com>
2017-08-10 01:38:43 -0700

    adding files

--- src/index.js
+++ src/index.js
@@ -1,25 +1,29 @@
 const Realm = require('realm')
+const faker = require('faker')
 
-const ADMIN_USERNAME = "admin.user@realm.io" // replace me!
-const AMDIN_PASSWORD = "ilovesushi" // replace me!
-const REALM_URL = "realm://localhost:9080" // replace me!
-const REALM_AUTH_URL = "http://localhost:9080" // replace me!
-const PRODUCTS_REALM_URL = `${REALM_URL}/products`
-
-function ready(realm){
-    const results = realm.objects('Products').filtered('replaceme!')
+const ProductSchema = {
+    name: 'Product',
+    primaryKey: 'productId',
+    properties: {
+        productId: { type: 'int', optional: false },
+        name: { type: 'string', optional: false },
+        price: { type: 'float', optional: false },
+        companyName: { type: 'string', optional: false },
+    }
 }
 
-Realm.Sync.User.login(REALM_AUTH, ADMIN_USERNAME, ADMIN_PASSWORD, (err, user) => {
-    if(err) { console.error(err.toString()) }
-    else {
-        Realm.openAsync({
-        user: user
-        }, (err, realm) => {
-            if (err) { console.error(err.toString()) }
-            else {
-                ready(realm)
-            }
-        })
-    }
-})
+for (var index = 0; index < 10000; index++) {
+    const realm = new Realm({
+        schema: [ProductSchema],
+        path: `./data/${index}.realm`
+    })
+    realm.write(() => {
+        realm.create('Product', {
+            productId: index,
+            name: faker.commerce.productName(),
+            price: faker.random.number({min: 20, max: 2000}),
+            companyName: faker.company.companyName()
+        }, true)
+    })
+    realm.close()
+}
